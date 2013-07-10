bca66aaa52fe4a05592488eef231dbc7a611d651
Xavier Ruiz <xavier.ruiz@cronologic.com>
2013-07-10 18:31:12 +0000

    Change to read from file index.html

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var BUF_LEN = 512;
+  var fs = require('fs');
+  var buf = new Buffer(BUF_LEN);
+
+  fd = fs.openSync('index.html');
+  pos = 0;
+  pos += fs.readSync(fd,buf,pos,BUF_LEN);
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
